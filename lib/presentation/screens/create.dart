d89cffd35c71989a19d43fb94d3a123bef04cca4
Dimitris Mylonas <gnout@hotmail.com>
2024-04-24 14:00:54 +0200

    Create credentials

--- lib/presentation/screens/create.dart
+++ lib/presentation/screens/create.dart
@@ -1,16 +1,169 @@
 import 'package:flutter/material.dart';
 
-class Create extends StatelessWidget {
+class Create extends StatefulWidget {
   const Create({super.key});
+
+  @override
+  State<Create> createState() => _CreateState();
+}
+
+class _CreateState extends State<Create> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _firstName = '';
+  String _lastName = '';
+  String _nationality = '';
+  String _dateOfBirth = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Berlin Hackathon'),
+        title: const Text('Create Credentials'),
       ),
-      body: const Center(
-        child: Text('Create Passport Credential'),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(
+            16.0,
+          ),
+          child: Column(
+            children: [
+              TextFormField(
+                validator: (val) {
+                  if (val == null) {
+                    return 'First name cannot be null';
+                  }
+
+                  if (val.isEmpty) {
+                    return 'First name cannot be empty';
+                  }
+
+                  return null;
+                },
+                keyboardType: TextInputType.name,
+                decoration: const InputDecoration(
+                  hintText: 'First name',
+                  prefixIcon: Icon(Icons.person),
+                ),
+                onChanged: (text) {
+                  _firstName = text;
+                },
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              TextFormField(
+                validator: (val) {
+                  if (val == null) {
+                    return 'Last name cannot be null';
+                  }
+
+                  if (val.isEmpty) {
+                    return 'Last name cannot be empty';
+                  }
+
+                  return null;
+                },
+                keyboardType: TextInputType.name,
+                decoration: const InputDecoration(
+                  hintText: 'Last name',
+                  prefixIcon: Icon(Icons.person),
+                ),
+                onChanged: (text) {
+                  _lastName = text;
+                },
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              TextFormField(
+                validator: (val) {
+                  if (val == null) {
+                    return 'Nationality cannot be null';
+                  }
+
+                  if (val.isEmpty) {
+                    return 'Nationality cannot be empty';
+                  }
+
+                  return null;
+                },
+                keyboardType: TextInputType.text,
+                decoration: const InputDecoration(
+                  hintText: 'Nationality',
+                  prefixIcon: Icon(Icons.flag),
+                ),
+                onChanged: (text) {
+                  _nationality = text;
+                },
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              TextFormField(
+                validator: (val) {
+                  if (val == null) {
+                    return 'Date of Birth cannot be null';
+                  }
+
+                  if (val.isEmpty) {
+                    return 'Date of Birth cannot be empty';
+                  }
+
+                  return null;
+                },
+                keyboardType: TextInputType.text,
+                decoration: const InputDecoration(
+                  hintText: 'Date of Birth',
+                  prefixIcon: Icon(Icons.date_range_outlined),
+                ),
+                onChanged: (text) {
+                  _dateOfBirth = text;
+                },
+              ),
+              const SizedBox(
+                height: 32.0,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  FocusManager.instance.primaryFocus?.unfocus();
+
+                  if (!_formKey.currentState!.validate()) {
+                    return;
+                  }
+
+                  String message = '';
+
+                  try {
+                    //
+                    // Do funny stuff here
+                    //
+
+                    message = 'Credential created successfully';
+                  } catch (e) {
+                    message = '$e';
+                  }
+
+                  if (context.mounted) {
+                    SnackBar snackBar = SnackBar(
+                      content: Text(message),
+                    );
+
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(48),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6.0),
+                  ),
+                ),
+                child: const Text('Create'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
